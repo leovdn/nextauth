00ae93bb02b3822e14df916abd9a821d5eea654f
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-05-24 22:49:16 -0300

    feat: create AuthContext

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,44 @@
 import type { NextPage } from "next"
+import { FormEvent, useContext, useState } from "react"
+import { AuthContext } from "../contexts/AuthContext"
 
 import styles from "../styles/Home.module.css"
 
 const Home: NextPage = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const { signIn } = useContext(AuthContext)
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    const data = {
+      email,
+      password,
+    }
+
+    await signIn(data)
+  }
+
   return (
-    <div className={styles.container}>
-      <h1>Next Auth</h1>
-    </div>
+    <form onSubmit={handleSubmit} className={styles.container}>
+      <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button type="submit">Entrar</button>
+    </form>
   )
 }
 
