c094054de95aaec04f7171bf39a3e1052872ddc6
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-05-27 13:12:19 -0300

    feat: hook to deal permissions

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan"
 import { setupApiClient } from "../services/api"
 import { api } from "../services/apiClient"
 import { WithSSRAuth } from "../utils/withSSRAuth"
@@ -7,12 +8,22 @@ import { WithSSRAuth } from "../utils/withSSRAuth"
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
 
+  const userCanSeeMetrics = useCan({
+    roles: ["administrator", "editor"],
+  })
+
   useEffect(() => {
     api.get("/me").then((response) => console.log(response))
     // .catch((error) => console.log(error))
   }, [])
 
-  return <h1>Dashboard: {user?.email}</h1>
+  return (
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
+  )
 }
 
 export const getServerSideProps = WithSSRAuth(async (ctx) => {
